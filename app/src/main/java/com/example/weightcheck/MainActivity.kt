6d5cd7ba14c3dbe41aead1c93adbe3fc392a93dc
Santiago Sáchica <ssachica@gmail.com>
2024-01-08 21:50:43 -0500

    -Change in the background color of the main activity -There is a new activity to show the results of the IMC (still in development) -Functional calculate button that redirects you to the activity_result

--- app/src/main/java/com/example/weightcheck/MainActivity.kt
+++ app/src/main/java/com/example/weightcheck/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.weightcheck
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
+
+    //Declaración de variables
+    private lateinit var buttonCalculate: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         //Declaramos el splash screen
         val splashScreen = installSplashScreen()
@@ -14,5 +20,24 @@ class MainActivity : AppCompatActivity() {
 
         //Inicializamos el splash screen
         splashScreen.setKeepOnScreenCondition { false }
+
+
+        //Llamamos las funciones necesarias
+        initComponents()
+        initListeners()
+    }
+
+    //Inicializamos los componentes
+    private fun initComponents(){
+        buttonCalculate = findViewById(R.id.buttonCalculate)
+    }
+
+    //Inicializamos los listeners
+    private fun initListeners(){
+        //Botón calcular
+        buttonCalculate.setOnClickListener {
+            val intent = Intent(this, activity_result::class.java)
+            startActivity(intent)
+        }
     }
 }
