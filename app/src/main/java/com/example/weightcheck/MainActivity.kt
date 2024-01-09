a5b0475a27e0a2d8a178bbe158f6e972b4a8405e
Santiago Sáchica <ssachica@gmail.com>
2024-01-09 15:00:43 -0500

    There is a new pop-up that appears when you open the app. That pop-up shows an alert about the precision of the IMC and says from what age is available

--- app/src/main/java/com/example/weightcheck/MainActivity.kt
+++ app/src/main/java/com/example/weightcheck/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
@@ -21,23 +22,38 @@ class MainActivity : AppCompatActivity() {
         //Inicializamos el splash screen
         splashScreen.setKeepOnScreenCondition { false }
 
-
         //Llamamos las funciones necesarias
         initComponents()
         initListeners()
     }
 
     //Inicializamos los componentes
-    private fun initComponents(){
+    private fun initComponents() {
+        showImcInfoDialog()
         buttonCalculate = findViewById(R.id.buttonCalculate)
     }
 
     //Inicializamos los listeners
-    private fun initListeners(){
+    private fun initListeners() {
         //Botón calcular
         buttonCalculate.setOnClickListener {
             val intent = Intent(this, activity_result::class.java)
             startActivity(intent)
         }
+    }
+
+    //Mostrar alerta de información acerca del IMC (váliido en adultos)
+    private fun showImcInfoDialog() {
+        val alertDialog = AlertDialog.Builder(this)
+
+        alertDialog.apply {
+            setIcon(R.drawable.bell)
+            setTitle("AVISO!")
+            setMessage("El IMC no es un cálculo 100% preciso, ya que no diagnostica la " +
+                    "grasa corporal ni la salud de un individuo. El IMC es una fórmula que relaciona " +
+                    "el peso y la altura de una persona, y se utiliza para clasificar el estado " +
+                    "nutricional según la Organización Mundial de la Salud. Además, el cálculo aquí " +
+                    "presente se limita a personas mayores de 20 años.")
+        }.create().show()
     }
 }
